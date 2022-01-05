9483adf1fc8c2c831545218205bf678235bc1a7d
Mila Votradovec <me@milavotradovec.cz>
2022-01-05 15:10:17 +0100

    refactor: specific functions

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use bytes::{Buf, Bytes};
 use std::fs::File;
 use std::io::prelude::*;
 
+const MAX: u16 = 2 << 14;
+
 fn main() {
-    let mut memory: [u16; 2 << 14] = [0; 2 << 14];
+    let mut memory: [u16; MAX as usize] = [0; MAX as usize];
     let mut registers: [u16; 8] = [0; 8];
     let mut stack: Vec<u16> = vec![];
 
@@ -22,104 +24,77 @@ fn main() {
     // Run program
     let mut ins_pointer = 0;
     loop {
-        let mut ins = memory[ins_pointer];
-        //dbg!(ins, ins_pointer, memory[ins_pointer], memory[ins_pointer+1], memory[ins_pointer+2]);
-        if is_register(ins) {
-            ins = registers[(ins - (2 << 14)) as usize]
-        }
+        let ins = get_value(memory[ins_pointer], &registers);
         let mut a = memory[ins_pointer + 1];
         //dbg!(ins, ins_pointer, memory[ins_pointer], memory[ins_pointer+1], memory[ins_pointer+2]);
-                //dbg!(registers);
+        //dbg!(registers);
         match ins {
+            // halt
             0 => break,
+            // set
             1 => {
                 //dbg!(registers);
                 //dbg!(ins_pointer, memory[ins_pointer+1], memory[ins_pointer+2]);
-                let reg = a - (2 << 14);
-                let mut b = memory[ins_pointer + 2];
-                if is_register(b) {
-                    b = registers[(b - (2 << 14)) as usize]
-                }
-                registers[reg as usize] = b;
+                a = a - (MAX);
+                let b = get_value(memory[ins_pointer + 2], &registers);
+                registers[a as usize] = b;
                 ins_pointer += 3;
                 //dbg!(registers);
             }
+            // push
             2 => {
-                if is_register(a) {
-                    a = registers[(a - (2 << 14)) as usize]
-                }
+                a = get_value(a, &registers);
                 stack.push(a);
                 ins_pointer += 2;
             }
+            // pop
             3 => {
                 let res = stack.pop().unwrap();
-                //dbg!(res);
-                if is_register(a) {
-                    registers[(a - (2 << 14)) as usize] = res;
-                } else {
-                    memory[a as usize] = res;
-                }
+                set_value(res, a, &mut registers, &mut memory).unwrap();
                 ins_pointer += 2;
             }
+            // eq
             4 => {
-                let mut b = memory[ins_pointer+2];
-                let mut c = memory[ins_pointer+3];
-                if is_register(b) {
-                    b = registers[(b - (2 << 14)) as usize]
-                }
-                if is_register(c) {
-                    c = registers[(c - (2 << 14)) as usize]
-                } 
-                let res = (b==c) as u16;
-                if is_register(a) {
-                    registers[(a - (2 << 14)) as usize] = res;
-                } else {
-                   memory[a as usize] = res; 
-                }
+                let b = get_value(memory[ins_pointer + 2], &registers);
+                let c = get_value(memory[ins_pointer + 3], &registers);
+                let res = (b == c) as u16;
+                set_value(res, a, &mut registers, &mut memory).unwrap();
                 ins_pointer += 4;
             }
+            // jmp
             6 => ins_pointer = a as usize,
+            // jt
             7 => {
-                if is_register(a) {
-                    a = registers[(a - (2 << 14)) as usize]
-                }
+                a = get_value(a, &registers);
                 if a > 0 {
                     ins_pointer = memory[ins_pointer + 2] as usize;
                 } else {
                     ins_pointer += 3;
                 }
             }
+            // jf
             8 => {
-                if is_register(a) {
-                    a = registers[(a - (2 << 14)) as usize]
-                }
+                a = get_value(a, &registers);
                 if a == 0 {
                     ins_pointer = memory[ins_pointer + 2] as usize;
                 } else {
                     ins_pointer += 3;
                 }
             }
+            // add
             9 => {
-                let mut b = memory[ins_pointer+2];
-                let mut c = memory[ins_pointer+3];
-                if is_register(b) {
-                    b = registers[(b - (2 << 14)) as usize]
-                }
-                if is_register(c) {
-                    c = registers[(c - (2 << 14)) as usize]
-                } 
-                let res = (b + c) % (2<<14);
-                if is_register(a) {
-                    registers[(a - (2 << 14)) as usize] = res;
-                } else {
-                   memory[a as usize] = res; 
-                }
+                let b = get_value(memory[ins_pointer + 2], &registers);
+                let c = get_value(memory[ins_pointer + 3], &registers);
+                let res = (b + c) % (MAX);
+                set_value(res, a, &mut registers, &mut memory).unwrap();
                 ins_pointer += 4;
             }
+            // out
             19 => {
                 print!("{}", memory[ins_pointer + 1] as u8 as char);
                 ins_pointer += 2
             }
+            // noop
             21 => ins_pointer += 1,
             _ => ins_pointer += 1,
         }
@@ -127,5 +102,27 @@ fn main() {
 }
 
 fn is_register(i: u16) -> bool {
-    i >= 2 << 14
+    i >= MAX
+}
+
+fn get_value(val: u16, registers: &[u16; 8]) -> u16 {
+    if is_register(val) {
+        return registers[(val - MAX) as usize];
+    }
+    return val;
+}
+
+fn set_value(
+    val: u16,
+    addr: u16,
+    registers: &mut [u16; 8],
+    memory: &mut [u16; MAX as usize],
+) -> Result<(), ()> {
+    if is_register(addr) {
+        registers[(addr - MAX) as usize] = val;
+    } else {
+        memory[addr as usize] = val;
+    }
+
+    return Ok(());
 }
